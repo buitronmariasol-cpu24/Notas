266e1772fae17b42f88307399aea0d007343b53b
Michelle Altamirano <michelle.altamirano@udla.edu.ec>
2026-06-09 11:49:48 -0500

    Segundo commit:adaptar_cargarNotas para recibir structs usando punteros

--- notas.c
+++ notas.c
@@ -13,3 +13,32 @@ typedef struct {
     RegistroAcademico academico;
 } Estudiante;
 
+// 2. Prototipos de funciones que reciben punteros a la estructura
+void cargarNotas(Estudiante *estudiantes, int total_estudiantes);
+void promediosPorEstudiante(Estudiante *estudiantes, int total_estudiantes);
+void promediosPorAsignatura(Estudiante *estudiantes, int total_estudiantes);
+void altaBajaPorEstudiante(Estudiante *estudiantes, int total_estudiantes);
+void altaBajaPorAsignatura(Estudiante *estudiantes, int total_estudiantes);
+void estadisticasAprobacion(Estudiante *estudiantes, int total_estudiantes);
+
+int main() {
+    int total_estudiantes;
+    
+    printf("SISTEMA DE GESTION DE CALIFICACIONES\n");
+    printf("Cantidad de estudiantes a registrar: ");
+    scanf("%d", &total_estudiantes);
+
+    // Creamos el arreglo de estructuras
+    Estudiante listaEstudiantes[total_estudiantes];
+
+    // Pasamos el arreglo (que decae en puntero) a las funciones
+    cargarNotas(listaEstudiantes, total_estudiantes);
+    promediosPorEstudiante(listaEstudiantes, total_estudiantes);
+    promediosPorAsignatura(listaEstudiantes, total_estudiantes);
+    altaBajaPorEstudiante(listaEstudiantes, total_estudiantes);
+    altaBajaPorAsignatura(listaEstudiantes, total_estudiantes);
+    estadisticasAprobacion(listaEstudiantes, total_estudiantes);
+
+    return 0;
+}
+
